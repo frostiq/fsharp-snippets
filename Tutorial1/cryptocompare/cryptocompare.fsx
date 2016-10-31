a4d5d5b7d87ecc60c2d22f28331317d35db0a04d
sayshay <bazhanau@outlook.com>
2016-10-31 10:16:34 +0300

    improvement

--- Tutorial1/cryptocompare/cryptocompare.fsx
+++ Tutorial1/cryptocompare/cryptocompare.fsx
@@ -4,8 +4,7 @@ open FSharp.Data
 
 let url = sprintf "https://www.cryptocompare.com/api/data/price?fsym=%s&tsyms=%s"
 type CryptoCompare = JsonProvider<"https://www.cryptocompare.com/api/data/price?fsym=BTC&tsyms=USD">
-let loadRate fromCur toCur = CryptoCompare.Load(url fromCur toCur).Data.[0].Price
+let loadRate fromCur toCur = if fromCur <> toCur then CryptoCompare.Load(url fromCur toCur).Data.[0].Price else 1.0M
 let getPaths fromCur throughCurrs toCur = throughCurrs |> Seq.map (fun t -> (loadRate fromCur t) * (loadRate t toCur))
 
-loadRate "ZEC" "USD"
 getPaths "ZEC" ["ETH"; "BTC"; "ZEC"] "USD"
